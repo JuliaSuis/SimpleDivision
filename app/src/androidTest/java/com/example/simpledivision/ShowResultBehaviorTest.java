366599cbc729b8d392fe3335a0bb26edc9edac7b
JuliaSuis <jeys@yandex.ru>
2019-03-10 23:59:37 +0100

    New tests to Espresso(Nan, Inf, Error)

--- app/src/androidTest/java/com/example/simpledivision/ShowResultBehaviorTest.java
+++ app/src/androidTest/java/com/example/simpledivision/ShowResultBehaviorTest.java
@@ -17,19 +17,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-/*import androidx.test.espresso.ViewAction;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnitRunner;
-
-import android.support.test.runner.AndroidJUnit4;*/
-/*import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;*/
-
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -39,22 +26,49 @@ public class ShowResultBehaviorTest {
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
     private String firstValueToBetyped;
     private String secondValueToBetyped;
+    private String zeroValuetoBetyped;
+    private String noValuetoBetyped;
 
     @Before
     public void initValidString() {
-        //InstrumentationRegistry.getTargetContext();
-        //MainActivity mainActivity = new MainActivity();
 
         firstValueToBetyped = "10";
         secondValueToBetyped = "5";
+        zeroValuetoBetyped = "0";
+        noValuetoBetyped = "";
     }
 
     @Test
-    public void showResult() {
+    public void showResultForInteger() {
         onView(withId(R.id.first)).perform(typeText(firstValueToBetyped));
         onView(withId(R.id.second)).perform(typeText(secondValueToBetyped));
         onView(withId(R.id.result_button)).perform(click());
         onView(withText("2.00")).check(matches(isDisplayed()));
 
     }
+
+    @Test
+    public void showResultForNan() {
+        onView(withId(R.id.first)).perform(typeText(zeroValuetoBetyped));
+        onView(withId(R.id.second)).perform(typeText(zeroValuetoBetyped));
+        onView(withId(R.id.result_button)).perform(click());
+        onView(withText("Nan result")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showResultForInf() {
+        onView(withId(R.id.first)).perform(typeText(firstValueToBetyped));
+        onView(withId(R.id.second)).perform(typeText(zeroValuetoBetyped));
+        onView(withId(R.id.result_button)).perform(click());
+        onView(withText("Inf result")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showResultForEmptyFields() {
+        onView(withId(R.id.first)).perform(typeText(noValuetoBetyped));
+        onView(withId(R.id.second)).perform(typeText(secondValueToBetyped));
+        onView(withId(R.id.result_button)).perform(click());
+        onView(withText("Err: enter valid numbers.")).check(matches(isDisplayed()));
+    }
+
 }
